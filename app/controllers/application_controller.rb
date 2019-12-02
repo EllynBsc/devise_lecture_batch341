06e6f4b97b592440de916e023a0684c1a012dda7
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-12-02 10:17:12 +0100

    lecture devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! #will check if the user is authenticated, if its is you can run the action of the controller otherwise you have to sign up/log in
 end
